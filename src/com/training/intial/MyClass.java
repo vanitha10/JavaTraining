f9dceac0f316a433cef5124d41563df25046edea
harsh <harsh@VanithaWork.lan>
2022-04-11 14:58:15 -0500

    Initial Commit

---
+++ src/com/training/intial/MyClass.java
@@ -0,0 +1,12 @@
+package com.training.intial;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		System.out.println("this is java file");
+		
+	
+		
+	}
+
+}
---
+++ src/com/training/intial/Person.java
@@ -0,0 +1,18 @@
+package com.training.intial;
+
+public class Person {
+
+	
+	int height;
+	int weight;
+	public String getPerosnInfo()
+	{
+		return
+				
+				
+				syso
+	}
+
+	}
+
+}
